5b99e279348d188352a0090969cb4d8a83b4f8c2
Anwarkamal143 <anwarkamal1434@gmail.com>
2026-08-08 23:25:56 +0500

    updated all package json files and fixes dependency issues

---
+++ .changeset/better-apples-cover.md
@@ -0,0 +1,10 @@
+---
+"@auth-core/hashing": patch
+"@auth-core/memory": patch
+"@auth-core/shared": patch
+"@auth-core/redis": patch
+"@auth-core/core": patch
+"@auth-core/jwt": patch
+---
+
+updated package json files and fixes dependency issues
--- packages/hashing/src/index.ts
+++ packages/hashing/src/index.ts
@@ -3,5 +3,5 @@ export * from "./password-service.js";
 export * from "./password-strength.js";
 export { createArgon2Driver } from "./drivers/argon2.js";
 export type { Argon2DriverOptions } from "./drivers/argon2.js";
-export { createBcryptDriver } from "./drivers/bcrypt.js";
-export type { BcryptDriverOptions } from "./drivers/bcrypt.js";
+// export { createBcryptDriver } from "./drivers/bcrypt.js";
+// export type { BcryptDriverOptions } from "./drivers/bcrypt.js";
